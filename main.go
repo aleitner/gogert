eb2793ece898435446fb2ace81038fba55071752
Alexander Leitner <leitnersalex@gmail.com>
2019-04-29 16:35:44 -0400

    Move stuff around to get tests running

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package gogert
 
 import (
 	"bytes"
@@ -77,77 +77,6 @@ func main() {
 		// recursively Iterate over each field and interpolate name and respective types to c definitions
 
 	}
-}
-
-func fromGoType(gotype string) string {
-	gotypeWithoutPtr, ptrRef := separatePtr(gotype)
-
-	if strings.HasPrefix(gotypeWithoutPtr, "[]") {
-		return fromSliceType(gotypeWithoutPtr) + ptrRef
-	}
-
-	if strings.HasPrefix(gotypeWithoutPtr, "map") {
-		return fromMapType(gotypeWithoutPtr) + ptrRef
-	}
-
-	switch gotypeWithoutPtr {
-	case "string":
-		return "uint8_t*" + ptrRef
-	case "bool":
-		return "bool" + ptrRef
-	case "int8":
-		return "int8_t" + ptrRef
-	case "uint8":
-		return "uint8_t" + ptrRef
-	case "byte":
-		return "uint8_t" + ptrRef
-	case "int16":
-		return "int16_t" + ptrRef
-	case "uint16":
-		return "uint16_t" + ptrRef
-	case "int32":
-		return "int32_t" + ptrRef
-	case "rune":
-		return "int32_t" + ptrRef
-	case "uint32":
-		return "uint32_t" + ptrRef
-	case "int64":
-		return "int64_t" + ptrRef
-	case "uint64":
-		return "uint64_t" + ptrRef
-	case "float32":
-		return "float" + ptrRef
-	case "float64":
-		return "double" + ptrRef
-	case "int":
-		return "int" + ptrRef
-	case "uint":
-		return "unsigned int" + ptrRef
-	case "uintptr": //TODO: This is platform dependent
-		return "uint64_t" + ptrRef
-	default:
-		return "void" + ptrRef
-	}
-}
-
-func separatePtr(gotype string) (newgotype string, ptr string) {
-	re, _ := regexp.Compile(`(^\**)+`)
-	matches := re.FindStringSubmatch(gotype)
-	if len(matches) > 1 {
-		return gotype[len(matches[1]):], matches[1]
-	}
-
-	return gotype, ""
-}
-
-func fromMapType(gotype string) string {
-	return gotype
-}
-
-func fromSliceType(gotype string) string {
-	gotype = gotype[2:]
-	ctype := fromGoType(gotype)
-	return ctype + "*"
 }
 
 func generateStruct(fset *token.FileSet, w io.Writer, name string, fields *ast.FieldList) error {
--- typeConversion_test.go
+++ typeConversion_test.go
@@ -1,4 +1,4 @@
-package main
+package gogert
 
 import (
 	"os"
@@ -13,26 +13,26 @@ var conversions = []struct {
 	{"*B", "void*"},   // pointer struct
 	{"**B", "void**"}, // pointer to a pointer of a struct
 
-	{"int64", "uint64_t"},    // basic type
+	{"int64", "int64_t"},     // basic type
 	{"string", "uint8_t*"},   // basic type
 	{"float32", "float"},     // basic type
 	{"float64", "double"},    // basic type
 	{"int", "int"},           // basic type
 	{"uint", "unsigned int"}, // basic type
-	{"*int64", "uint64_t*"},  // pointer to basic type
+	{"*int64", "int64_t*"},   // pointer to basic type
 
 	{"memory.Size", "void"}, // custom basic type
 
-	{"[]int64", "uint64_t*"},
-	{"[][]int64", "uint64_t**"},
-	{"*[]int64", "uint64_t***"},
-	{"[][]*int64", "uint64_t***"},
-	{"[]*[]int64", "uint64_t***"},
+	{"[]int64", "int64_t*"},
+	{"[][]int64", "int64_t**"},
+	{"*[]int64", "int64_t**"},
+	{"[][]*int64", "int64_t***"},
+	{"[]*[]int64", "int64_t***"},
 	{"map[string]*Event", ""},
 	{"map[string]map[string]*Event", ""},
 }
 
-func TestfromGoType(t *testing.T) {
+func TestFromGoType(t *testing.T) {
 
 	for _, tt := range conversions {
 		actual := fromGoType(tt.gotype)
